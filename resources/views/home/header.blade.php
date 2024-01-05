3693e8e9e8e1c6db05f00a98bf6758f82610c8b3
jurgen <133093619+Jpjoker@users.noreply.github.com>
2024-01-05 18:29:44 +0100

    profiel system on homepage done

--- resources/views/home/header.blade.php
+++ resources/views/home/header.blade.php
@@ -7,9 +7,21 @@
             <li><a href="/services">Services</a></li>
             <li><a href="/about-us">About Us</a></li>
             <li><a href="/blog">Blog</a></li>
-            <li><a href="/register"> Sign Up </a></li>
-            <li><a href="/login">Log In</a></li>
+
+            @if (Route::has('login'))
+                @auth
+                    <li> <x-app-layout>
+
+                        </x-app-layout> </li>
+                @else
+                    <li><a href="{{ route('login') }}">Login</a></li>
+                    <li><a href="{{ route('register') }}">Sign Up </a></li>
+                @endauth
+
         </ul>
+
     </nav>
+    @endif
     <a class="cta" href="/contact"><button>Contact</button></a>
+
 </header>
